2b0a67c99d9ab8b55611a173134fd20fc9560f2e
Kain Shepard <kainkun100@gmail.com>
2020-05-28 22:12:27 -0700

    MockHMD

---
+++ Assets/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    public static WeatherController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    Transform[] airpockets;
+
+    void Start()
+    {
+        airpockets = GetComponentsInChildren<Transform>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        foreach (Transform airpocket in airpockets)
+        {
+            Gizmos.color = new Color(0.2f, 0.2f, 1, 0.5f);
+            Gizmos.DrawCube(airpocket.position, airpocket.localScale);
+        }
+
+    }
+}
