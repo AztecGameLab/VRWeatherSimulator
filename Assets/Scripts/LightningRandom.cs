71ffa9bab22b45de6489f596efd1d3627a8394a9
Kyle McLain Kane <kmclain16@sbcglobal.net>
2020-06-06 21:25:34 -0700

    Someone test plz

---
+++ Assets/Scripts/LightningRandom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningRandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // These are the lightning balls
+    public GameObject g1;
+    public GameObject g2;
+    public GameObject g3;
+    public GameObject g4;
+    public GameObject g5;
+    public GameObject g6;
+    public GameObject g7;
+    public GameObject g8;
+
+    // Lightning position
+    public GameObject lightningPos;
+
+    void Start()
+    {
+        // Works off of the X,Y,Z system
+        // The only ones that should change position is the X and Z coordinate
+        lightningPos.transform.position = new Vector3(Random.Range(-20.0f, 20.0f), 0, Random.Range(-20.0f, 20.0f));
+        g1.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g2.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g3.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g3.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g4.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g5.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g6.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g7.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+        g8.transform.position = new Vector3(Random.Range(-5.0f, 5.0f), 0, Random.Range(-5.0f, 5.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
