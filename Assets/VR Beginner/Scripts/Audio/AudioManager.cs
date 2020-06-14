5fea70abb3c465c1e91091d6208c0bf4e2c46c06
DarkforcesII <darkforcesii@gmail.com>
2020-06-14 12:23:40 -0700

    Added Audio Manager

---
+++ Assets/VR Beginner/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            GameObject managerObj = new GameObject("AudioManager");
+            instance = managerObj.AddComponent<AudioManager>();
+            return instance;
+        }
+    }
+
+    // Initialized audio sources for manager
+    #region
+    private AudioSource musicSource;
+    private AudioSource sfxSource;
+    #endregion
+
+
+    private void Awake()
+    {
+        musicSource = this.gameObject.AddComponent<AudioSource>();
+        sfxSource = this.gameObject.AddComponent<AudioSource>();
+
+        // Assigning audio mixer child to each audio source
+        AudioMixer MasterMixer = Resources.Load<AudioMixer>("Master");
+        string _MixerGroup1 = "Music";
+        string _MixerGroup2 = "SFX";
+        musicSource.outputAudioMixerGroup = MasterMixer.FindMatchingGroups(_MixerGroup1)[0];
+        sfxSource.outputAudioMixerGroup = MasterMixer.FindMatchingGroups(_MixerGroup2)[0];
+    }
+
+    // Play one shot audio clips
+    public void PlaySFX(AudioClip audioClip, float volume, float pitch)
+    {
+        sfxSource.volume = volume;
+        sfxSource.pitch = pitch;
+        sfxSource.PlayOneShot(audioClip);
+    }
+
+    // Play music with option to loop
+    public void PlayMusic(AudioClip audioClip, float volume, bool loop = false)
+    {
+        musicSource.clip = audioClip;
+        musicSource.volume = volume;
+        musicSource.loop = loop;
+        musicSource.Play();
+    }
+
+    // Stop Music
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    // Pause Music
+    public void PauseMusic()
+    {
+        musicSource.Pause();
+    }
+
+    // change music volume
+    public void MusicVol(float volume)
+    {
+        musicSource.volume = volume;
+    }
+}
---
+++ Assets/VR Beginner/Scripts/Audio/NewScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public AudioClip[] audioClips;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            AudioManager.Instance.PlaySFX(audioClips[0], 1.0f, 1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.Instance.PlayMusic(audioClips[1], 1.0f, true);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            AudioManager.Instance.StopMusic();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            AudioManager.Instance.PauseMusic();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            AudioManager.Instance.MusicVol(0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            AudioManager.Instance.MusicVol(1.0f);
+        }
+    }
+}
